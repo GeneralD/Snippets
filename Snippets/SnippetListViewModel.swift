805875d3f26d321bfd0005e5dae9a676249df13e
GeneralD <yumejustice@gmail.com>
2020-05-05 19:40:12 +0900

    initial commit

---
+++ Snippets/SnippetListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SnippetListViewModel.swift
+//  Snippets
+//
+//  Created by Yumenosuke Koukata on 2020/04/30.
+//  Copyright © 2020 ZYXW. All rights reserved.
+//
+
+import Foundation
+
+class SnippetListViewModel {
+	
+}
