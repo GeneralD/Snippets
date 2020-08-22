b7c762a86ce02a7bea124ec90f13d8e1a49b7f58
GeneralD <yumejustice@gmail.com>
2020-08-23 04:42:04 +0900

    refactor extensions

--- Snippets/Sources/Extensions/Rx/UIKit/UIResponderReactiveExtension.swift
+++ Snippets/Sources/Extensions/Rx/UIKit/UIResponderReactiveExtension.swift
@@ -1,22 +1,22 @@
 //
-//  UIViewReactiveExtension.swift
+//  UIResponderReactiveExtension.swift
 //  Snippets
 //
 //  Created by Yumenosuke Koukata on 2020/08/07.
 //  Copyright © 2020 ZYXW. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import RxSwift
 import RxCocoa
 
-public extension Reactive where Base: UIView {
+public extension Reactive where Base: UIResponder {
 	var isFirstResponder: Binder<Bool> {
-		.init(self.base) { view, beFirstResponder in
+		.init(self.base) { responder, beFirstResponder in
 			if beFirstResponder {
-				view.becomeFirstResponder()
+				responder.becomeFirstResponder()
 			} else {
-				view.resignFirstResponder()
+				responder.resignFirstResponder()
 			}
 		}
 	}
