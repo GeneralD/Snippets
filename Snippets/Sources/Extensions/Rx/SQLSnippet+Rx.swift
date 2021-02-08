5394a24bea6ebf71b8f9df8b46bed0a91ba8915b
GeneralD <yumejustice@gmail.com>
2021-02-08 18:39:00 +0900

    Use Single instead of Observable at SQLSnippet+Rx

--- Snippets/Sources/Extensions/Rx/SQLSnippet+Rx.swift
+++ Snippets/Sources/Extensions/Rx/SQLSnippet+Rx.swift
@@ -14,30 +14,30 @@ import RxSwift
 extension SQLSnippet: ReactiveCompatible {}
 
 extension Reactive where Base: SQLSnippet {
-	func tags(url: URL) -> Observable<[String]> {
+	enum GRDBError: Error {
+		case databaseError
+	}
+
+	func tags(url: URL) -> Single<[String]> {
 		tags(path: url.absoluteString)
 	}
 
-	func tags(path: String) -> Observable<[String]> {
-		(try? DatabaseQueue(path: path)).asObservable()
-			.flatMap { dbQueue in
-				dbQueue.rx.read { db in
-					let tids = try SQLTagsIndex.filter(Column("sid") == self.base.sid).fetchAll(db).compactMap(\.tid)
-					return try SQLTag.filter(tids.contains(Column("tid"))).fetchAll(db).compactMap(\.tag)
-				}
-			}
+	func tags(path: String) -> Single<[String]> {
+		guard let queue = try? DatabaseQueue(path: path) else { return .error(GRDBError.databaseError) }
+		return queue.rx.read { db in
+			let tids = try SQLTagsIndex.filter(Column("sid") == self.base.sid).fetchAll(db).compactMap(\.tid)
+			return try SQLTag.filter(tids.contains(Column("tid"))).fetchAll(db).compactMap(\.tag)
+		}
 	}
 
-	static func all(url: URL) -> Observable<[SQLSnippet]> {
+	static func all(url: URL) -> Single<[SQLSnippet]> {
 		all(path: url.absoluteString)
 	}
 
-	static func all(path: String) -> Observable<[SQLSnippet]> {
-		(try? DatabaseQueue(path: path))
-			.asObservable()
-			.flatMap { dbQueue in dbQueue.rx.read { db in
-				try SQLSnippet.order(Column("syntax")).fetchAll(db)
-			}
-			}
+	static func all(path: String) -> Single<[SQLSnippet]> {
+		guard let queue = try? DatabaseQueue(path: path) else { return .error(GRDBError.databaseError) }
+		return queue.rx.read { db in
+			try SQLSnippet.order(Column("syntax")).fetchAll(db)
+		}
 	}
 }
