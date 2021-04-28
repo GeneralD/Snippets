5f9c9bd7da1a24a4993ab9cadffceb4c85579e30
GeneralD <yumejustice@gmail.com>
2021-04-28 23:45:01 +0900

    add Gemfile

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ which pod 1>/dev/null || brew install cocoapods
 # setup project
 mint bootstrap
 mint run xcodegen
+
+if which rbenv 1>/dev/null; then
+  rbenv exec gem install bundler
+  rbenv exec bundler install
+  rbenv exec bundler exec generamba template install
+fi
